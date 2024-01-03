76f6216167b9934d6b2a2ce7eae6797f188063c8
Avi <avi.leeker@gmail.com>
2024-01-03 19:28:42 +0200

    wip

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -10,11 +10,12 @@ interface IMainNavProps {
 }
 const MainNav: React.FC<IMainNavProps> = ({ data }) => {
   const pathname = usePathname();
-  const routes = data?.map((route) => ({
-    href: `/category/${route.id}`,
-    label: route.name,
-    active: pathname === `/category/${route.id}`,
-  }));
+  const routes =
+    data?.map((route) => ({
+      href: `/category/${route.id}`,
+      label: route.name,
+      active: pathname === `/category/${route.id}`,
+    })) || [];
   return (
     <nav className='mx-6 flex items-center space-x-4 lg:space-x-6'>
       {routes?.map((route) => (
