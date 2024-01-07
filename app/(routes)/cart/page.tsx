483f9718cc27a1c2e2b2a5494ec93692e6ef6b7d
Avi <avi.leeker@gmail.com>
2024-01-07 18:43:42 +0200

    wip

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import Container from "@/components/ui/container";
-// import useCart from "@/hooks/use-cart";
+import useCart from "@/hooks/use-cart";
 import useIsMounted from "@/hooks/use-is-mounted";
-// import CartItem from "./components/cart-item";
+import CartItem from "./components/cart-item";
 import Summary from "./components/summary";
 export const revalidate = 0;
 const CartPage = () => {
   const isMounted = useIsMounted();
-  //   const cart = useCart();
+  const cart = useCart();
   if (!isMounted) return null;
   return (
     <div className='bg-white'>
@@ -17,14 +17,14 @@ const CartPage = () => {
           <h1 className='text-3xl font-bold text-black'>Shopping Cart</h1>
           <div className='mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12'>
             <div className='lg:col-span-7'>
-              {/* {cart?.items?.length === 0 && (
+              {cart?.items?.length === 0 && (
                 <p className='text-neutral-500'>No items added to cart.</p>
-              )} */}
+              )}
               <ul>
-                {/* {cart?.items.length >= 1 &&
+                {cart?.items?.length >= 1 &&
                   cart?.items?.map((item) => (
                     <CartItem key={item?.id} data={item} />
-                  ))} */}
+                  ))}
               </ul>
             </div>
             <Summary />
